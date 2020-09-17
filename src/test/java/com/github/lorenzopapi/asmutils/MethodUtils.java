af2f1e279012b51fe2b2abe52045def47b668ef5
LorenzoPapi <42911751+LorenzoPapi@users.noreply.github.com>
2020-09-17 21:51:05 +0200

    method utils d o n e (I am getting better with asm yey)
    Also fixed constructor utils

--- src/test/java/com/github/lorenzopapi/asmutils/MethodUtils.java
+++ src/test/java/com/github/lorenzopapi/asmutils/MethodUtils.java
@@ -30,7 +30,17 @@ public class MethodUtils {
 		insns1.add(new FieldInsnNode(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;"));
 		insns1.add(new LdcInsnNode("e"));
 		insns1.add(new MethodInsnNode(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V"));
+		InsnList insns2 = new InsnList();
+		insns2.add(new FieldInsnNode(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;"));
+		insns2.add(new LdcInsnNode("e"));
+		insns2.add(new MethodInsnNode(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V"));
+		InsnList insns3 = new InsnList();
+		insns3.add(new FieldInsnNode(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;"));
+		insns3.add(new LdcInsnNode("a d d e d"));
+		insns3.add(new MethodInsnNode(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V"));
 		addInstructionsToStartOrEnd("hello", "()I", insns, true);
+		addMethodToClass(ACC_PUBLIC, "theWorld", "()V", null, null, insns2);
+		addInstructionsAfterOrBeforeInsn("theWorld", "()V", insns3, GETSTATIC, 1, 0, true);
 		byte[] bytes = addInstructionsToStartOrEnd("hello", "()I", insns1, false);
 		FileOutputStream stream = new FileOutputStream("yes.class");
 		stream.write(bytes);
@@ -63,6 +73,42 @@ public class MethodUtils {
 					});
 				}
 			}
+		ClassWriter result = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+		node.accept(result);
+		return result.toByteArray();
+	}
+
+	public static byte[] addMethodToClass(int access, String name, String descriptor, String signature, String[] ex, InsnList instructions) {
+		MethodNode method = new MethodNode(access, name, descriptor, signature, ex);
+		method.instructions = instructions;
+		node.methods.add(method);
+		ClassWriter result = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+		node.accept(result);
+		return result.toByteArray();
+	}
+
+	public static byte[] addInstructionsAfterOrBeforeInsn(String name, String desc, InsnList listToAdd, int opCodeToSearch, int position, int varInsnValue, boolean before) {
+		int insnCounter = 0;
+
+		for (MethodNode method : node.methods)
+			if (method.name.equals(name) && method.desc.equals(desc))
+				for (AbstractInsnNode actualInstruction : method.instructions)
+					if (actualInstruction.getOpcode() == opCodeToSearch) {
+						if ((opCodeToSearch >= 21 && opCodeToSearch <= 25) || (opCodeToSearch >= 54 && opCodeToSearch <= 58) || opCodeToSearch == 169) {
+							VarInsnNode varNode = (VarInsnNode) actualInstruction;
+							if (varNode.var == varInsnValue)
+								insnCounter++;
+						} else {
+							insnCounter++;
+						}
+						if (insnCounter == position) {
+							if (before) {
+								method.instructions.insertBefore(actualInstruction, listToAdd);
+							} else {
+								method.instructions.insert(actualInstruction, listToAdd);
+							}
+						}
+					}
 		ClassWriter result = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
 		node.accept(result);
 		return result.toByteArray();
