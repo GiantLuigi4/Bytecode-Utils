f9e3aa7405a7a506c3a5a3c15e1a3bcb7b914a45
LorenzoPapi <42911751+LorenzoPapi@users.noreply.github.com>
2020-09-17 09:35:16 +0200

    looks like everything works now

--- src/test/java/ConstructorUtils.java
+++ src/test/java/ConstructorUtils.java
@@ -1,11 +1,15 @@
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.*;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import static org.objectweb.asm.Opcodes.*;
+
+//Looks like everything works
 public class ConstructorUtils {
 
 	static ClassReader reader = null;
@@ -16,14 +20,23 @@ public class ConstructorUtils {
 		reader = new ClassReader("EmptyClass");
 		node = new ClassNode();
 		reader.accept(node, 0);
-		changeAccess(Opcodes.ACC_FINAL + Opcodes.ACC_PUBLIC);
+		changeAccess(ACC_FINAL + ACC_PUBLIC);
 		InsnList insns = new InsnList();
-		insns.add(new FieldInsnNode(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;"));
+		insns.add(new FieldInsnNode(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;"));
 		insns.add(new LdcInsnNode("EEEEEEEEEEEEEEEEEEEEEEEEEEE"));
-		insns.add(new MethodInsnNode(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V"));
-		//addInstructionsToStartOrEnd(insns, true);
-		addInstructionsToStartOrEnd(insns, false);
-		addInstructionsAfterOrBeforeInsn(insns, new MethodInsnNode(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V"), 0, 1, false);
+		insns.add(new MethodInsnNode(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V"));
+		InsnList insns1 = new InsnList();
+		insns1.add(new FieldInsnNode(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;"));
+		insns1.add(new LdcInsnNode("THE WORLD"));
+		insns1.add(new MethodInsnNode(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V"));
+		InsnList insns2 = new InsnList();
+		insns1.add(new FieldInsnNode(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;"));
+		insns1.add(new LdcInsnNode("THE WORLD"));
+		insns1.add(new MethodInsnNode(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V"));
+		addInstructionsToStartOrEnd(insns, "", true);
+		addInstructionsToStartOrEnd(insns1, "", false);
+		InsnNode beforeNode = new InsnNode(RETURN); //new MethodInsnNode(INVOKESPECIAL, "java/lang/Object", "<init>", "()V");
+		addInstructionsAfterOrBeforeInsn(insns2, beforeNode, 1, 0, true);
 		node.accept(result);
 		FileOutputStream stream = new FileOutputStream("yes.class");
 		stream.write(result.toByteArray());
@@ -36,18 +49,31 @@ public class ConstructorUtils {
 				method.access = newAccess;
 	}
 
-	public static void addInstructionsToStartOrEnd(InsnList list, boolean atStart) {
+	public static void addInstructionsToStartOrEnd(InsnList list, String descriptor, boolean atStart) {
+		if (descriptor.equals("")) {
+			descriptor = "()V";
+		}
 		for (MethodNode method : node.methods)
-			if ((method.name.equals("<clinit>") || method.name.equals("<init>")) && method.desc.contains(")V")) {
-				System.out.println(method.instructions.size());
-				if (atStart)
-					method.instructions.insert(list); //TODO FIX THIS, CUZ IT LITERALLY PUT INSNS BEFORE THE super() THING IN CONSTRUCTOR
-				else
-					method.instructions.insertBefore(new InsnNode(Opcodes.RETURN), list);
+			if ((method.name.equals("<clinit>") || method.name.equals("<init>")) && method.desc.contains(descriptor)) {
+				for (AbstractInsnNode node : method.instructions) {
+					List<Integer> opcodesList = new ArrayList<>();
+					method.instructions.forEach((absNode) -> opcodesList.add(absNode.getOpcode()));
+					if (atStart) {
+						if (opcodesList.contains(INVOKESPECIAL)) {
+							if (node.getOpcode() == INVOKESPECIAL) {
+								method.instructions.insert(node, list);
+							}
+						} else {
+							method.instructions.insert(list);
+						}
+					} else if (node.getOpcode() == RETURN) {
+						method.instructions.insertBefore(node, list);
+					}
+				}
 			}
 	}
 
-	public static void addInstructionsAfterOrBeforeInsn(InsnList listToAdd, AbstractInsnNode insn, int varInsnValue, int position, boolean before) { //I don't know if it works
+	public static void addInstructionsAfterOrBeforeInsn(InsnList listToAdd, AbstractInsnNode insn, int position, int varInsnValue, boolean before) { //I don't know if it works
 		int insnCounter = 0;
 
 		for (MethodNode method : node.methods)
@@ -61,13 +87,15 @@ public class ConstructorUtils {
 						} else {
 							insnCounter++;
 						}
-						if (insnCounter == position)
-							if (before)
+						if (insnCounter == position) {
+							if (before) {
 								method.instructions.insertBefore(insn, listToAdd);
-							else
+							} else {
 								method.instructions.insert(insn, listToAdd);
+							}
+						}
 					}
-		
+
 	}
 
 }
